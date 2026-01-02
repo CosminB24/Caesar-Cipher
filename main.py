dd768fa33f0fd529a0ebc509e5408b0ec98a29a7
CosminB24 <cosminbadea919@yahoo.com>
2026-01-02 02:45:59 +0200

    refactored version of the code

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import caesar_cipher as cc
 text = "PUXKN QJRA ANLNREN TRWPMXV"
 
 results = cc.decrypt_message(text)
+results = cc.encrypt_message(text)
 
+print("Brute force decryption:")
 for item in results:
     shift = item[0]
     candidate = item[1]
     print("shift =", shift, "message =", candidate)
 
-print("Specific shift: ")
+print("Encryption:")
+for item in results:
+    shift = item[0]
+    candidate = item[1]
+    print("shift =", shift, "message = ", candidate)
 
+print("Specific shift: ")
 print("decrypt shift 12:", cc.decrypt_message_with_specific_shift(text, 12))
 print("encrypt shift 12:", cc.encrypt_message_with_specific_shift(text, 12))
